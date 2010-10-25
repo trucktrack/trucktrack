c9ba6e108397daf741f95820076fceaa7b4954d4
Brian Eng <beng@vailsys.com>
2010-10-24 20:16:07 -0500

    intial commit

---
+++ Gemfile
@@ -0,0 +1,12 @@
+# A sample Gemfile
+source :gemcutter
+source 'http://gems.github.com'
+
+gem 'rails'
+
+group :development do
+  gem 'git'
+  gem 'mysql'
+#  gem 'ruby-debug'
+  gem 'rspec-rails', '~> 2.0.1'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Consumer::Application.config.secret_token = 'd418a9f8d0b60d1b874d8932937a6c37e96c6803ecc437ebf3a359fbf22aca4c56b4c577be3b0599876d46a69c7dab889fb71ee45b718c71f8f1c88d77790709'
